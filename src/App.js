878cf81ccf2a2d95db29217eee22c0ba8f3cf5f6
seba.brzovic <seba.brzovic@gmail.com>
2025-02-26 14:31:24 -0300

    agregado los creditos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import facturas from "./facturas.json";
+import { useState } from "react";
 
 console.log(facturas);
 
 function App() {
+  const [facturaElegida, setFacturaElegida] = useState(null);
+  const [creditos, setCreditos] = useState(null);
+  const [seleccionado, setSeleccionado] = useState(false);
   const facturasRecibidas = facturas.map((factura, index) => {
     if (factura.type === "received") {
       return (
@@ -20,14 +24,44 @@ function App() {
       );
     }
   });
+  const creditosRecibidos = facturas.map((factura, index)=>{
+    if (facturaElegida === factura.reference){
+      return (
+        <option value={factura.id}>
+            <tr>
+              <td>inv_{index + 1}</td>
+              <td>{factura.organization_id}</td>
+              <td>{factura.amount}</td>
+              <td>{factura.currency}</td>
+              <td>{facturaElegida}</td>
+            </tr>
+            </option>
+      )
+    }
 
+  })
+  console.log(facturaElegida);
+  console.log(seleccionado);
 
   return (
+    <div>
     <div className="App">
-      <h1>Facturas Recibidas</h1>
-      <select>{facturasRecibidas}</select>
+      <h1>Selecciona una factura</h1>
+      <select
+        value = {facturaElegida}
+        onChange={e => {setFacturaElegida(e.target.value); setSeleccionado(true)}}
+
+      >{facturasRecibidas}</select>
     </div>
-  );
+    <div className="App" style= {{display : seleccionado ? "block" : "none"}}>
+      <h1>Selecciona una nota de credito</h1>
+      <select
+        value = {creditos}
+        onChange={e => setCreditos(e.target.value)}
+      >{creditosRecibidos}</select>
+    </div>
+    </div>
+      );
 }
 
 export default App;
