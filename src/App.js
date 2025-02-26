c8faaf2b28d5b84a516ab0e59807befd6f3f59ca
seba.brzovic <seba.brzovic@gmail.com>
2025-02-26 16:01:43 -0300

    arreglo de grafica y cambio de select a checkbox

--- src/App.js
+++ src/App.js
@@ -13,65 +13,107 @@ function App() {
   const [creditos, setCreditos] = useState(null);
   const [seleccionado, setSeleccionado] = useState(false);
   const [creditoSeleccionado, setCreditoSeleccionado] = useState(false);
-  const facturasRecibidas = facturas.map((factura, index) => {
-    if (factura.type === "received") {
-      return (
-        <option value={factura.id} class="form-check-input" type="checkbox">
-            <tr>
-              <td>inv_{index + 1}</td>
-              <td>{factura.organization_id}</td>
-              <td>{factura.amount}</td>
-              <td>{factura.currency}</td>
-              <td>{factura.type}</td>
-            </tr>
-        </option>
-      );
-    }
-  });
-  const creditosRecibidos = facturas.map((factura, index)=>{
-    if (facturaElegida === factura.reference){
-      return (
-        <option value={factura.id}>
-            <tr>
-              <td>inv_{index + 1}</td>
-              <td>{factura.organization_id}</td>
-              <td>{factura.amount}</td>
-              <td>{factura.currency}</td>
-              <td>{facturaElegida}</td>
-            </tr>
-            </option>
-      )
-    }
 
-  })
+  const handleFacturaChange = (facturaId) => {
+    setFacturaElegida(facturaId);
+    setSeleccionado(true);
+  };
+
+  const handleCreditoChange = (creditoId) => {
+    setCreditos(creditoId);
+    setCreditoSeleccionado(true);
+  }
+  const facturaRecibidas = facturas.filter((factura) => factura.type === "received");
+  const creditosRecibidos = facturas.filter((factura) => facturaElegida === factura.reference);
+
   console.log(facturaElegida);
   console.log(seleccionado);
   console.log(creditoSeleccionado);
 
   return (
     <div>
-    <div className="App" class="form-check">
+    <div className="App" >
       <h1>Selecciona una factura</h1>
-      <select
-        value = {facturaElegida}
-        onChange={e => {setFacturaElegida(e.target.value); setSeleccionado(true)}}
+      <div className="table-responsive">
+      <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Seleccionar</th>
+            <th scope="col">ID</th>
+            <th scope="col">Organizacion</th>
+            <th scope="col">Monto</th>
+            <th scope="col">Moneda</th>
+            <th scope="col">Tipo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {facturaRecibidas.map((factura, index) => (
+            <tr key={factura.id} className="table-primary">
+              <td>
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  checked={facturaElegida === factura.id}
+                  value={factura.id}
+                  onChange={() => handleFacturaChange(factura.id)}
+                />
+              </td>
+              <td>{factura.id}</td>
+              <td>{factura.organization_id}</td>
+              <td>{factura.amount}</td>
+              <td>{factura.currency}</td>
+              <td>{factura.type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </div>
+    {seleccionado && (
+      <div className="App">
+        <h1>Selecciona una nota de credito</h1>
+        <div className="table-responsive">
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Seleccionar</th>
+              <th scope="col">ID</th>
+              <th scope="col">Organizacion</th>
+              <th scope="col">Monto</th>
+              <th scope="col">Moneda</th>
+              <th scope="col">Referencia</th>
+            </tr>
+          </thead>
+          <tbody>
+            {creditosRecibidos.map((factura, index) => (
+              <tr key={factura.id} className="table-primary">
+                <td>
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    value={factura.id}
+                    onChange={() => handleCreditoChange(factura.id)}
+                  />
+                </td>
+                <td>{factura.id}</td>
+                <td>{factura.organization_id}</td>
+                <td>{factura.amount}</td>
+                <td>{factura.currency}</td>
+                <td>{factura.reference}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+      </div>)}
 
-      >{facturasRecibidas}</select>
-    </div>
-    <div className="App" style= {{display : seleccionado ? "block" : "none"}}>
-      <h1>Selecciona una nota de credito</h1>
-      <select
-        value = {creditos}
-        onChange={e => {setCreditos(e.target.value); setCreditoSeleccionado(true)}}
-      >{creditosRecibidos}</select>
-    </div>
     
       <Modal show={creditoSeleccionado}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Footer>
-          <Button variant="primary" onClick={e => {setCreditoSeleccionado(false); setSeleccionado (false)}}>Seguir Asignando</Button>
+          <Button variant="primary" onClick={e => {setCreditoSeleccionado(false); setSeleccionado (false); setFacturaElegida(null); setCreditos(null)}}>Seguir Asignando</Button>
         </Modal.Footer>
       </Modal>
     </div>
