bad5badb38d6b701f46120ca0904fe3aad4f9995
seba.brzovic <seba.brzovic@gmail.com>
2025-02-26 15:16:28 -0300

    assignar listo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from "./logo.svg";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import facturas from "./facturas.json";
 import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 console.log(facturas);
 
@@ -9,10 +12,11 @@ function App() {
   const [facturaElegida, setFacturaElegida] = useState(null);
   const [creditos, setCreditos] = useState(null);
   const [seleccionado, setSeleccionado] = useState(false);
+  const [creditoSeleccionado, setCreditoSeleccionado] = useState(false);
   const facturasRecibidas = facturas.map((factura, index) => {
     if (factura.type === "received") {
       return (
-        <option value={factura.id}>
+        <option value={factura.id} class="form-check-input" type="checkbox">
             <tr>
               <td>inv_{index + 1}</td>
               <td>{factura.organization_id}</td>
@@ -42,10 +46,11 @@ function App() {
   })
   console.log(facturaElegida);
   console.log(seleccionado);
+  console.log(creditoSeleccionado);
 
   return (
     <div>
-    <div className="App">
+    <div className="App" class="form-check">
       <h1>Selecciona una factura</h1>
       <select
         value = {facturaElegida}
@@ -57,9 +62,18 @@ function App() {
       <h1>Selecciona una nota de credito</h1>
       <select
         value = {creditos}
-        onChange={e => setCreditos(e.target.value)}
+        onChange={e => {setCreditos(e.target.value); setCreditoSeleccionado(true)}}
       >{creditosRecibidos}</select>
     </div>
+    
+      <Modal show={creditoSeleccionado}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button variant="primary" onClick={e => {setCreditoSeleccionado(false); setSeleccionado (false)}}>Seguir Asignando</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
       );
 }
